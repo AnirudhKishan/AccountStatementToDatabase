f9457dfdbc87bf416a9b6fb1e232d17412487866
Fa773N M0nK <Fa773N.M0nK@gmail.com>
2017-06-03 20:50:07 +0530

    ExistsInDB() now taked both transactions tables into consideration

--- src/HdfcSqliteDatabaseWriter.py
+++ src/HdfcSqliteDatabaseWriter.py
@@ -36,14 +36,22 @@ class HdfcSqliteDatabaseWriter(DatabaseWriter):
 		self.Repository.ExecuteCommand(SqlCommand(query_2, params_2))
 
 	def ExistsInDb(self, hdfcTransaction):
-		query_1 = "SELECT * FROM 'Transactions' WHERE [Timestamp]=:timestamp AND [Amount]=:amount AND [Description]=:description;"
-		
+		query_1 = "SELECT * FROM [Transactions] WHERE [Timestamp]=:timestamp AND [Amount]=:amount AND [Description]=:description;"
 		params_1 = {
 			'timestamp': hdfcTransaction.Timestamp.isoformat(),
 			'amount': str(hdfcTransaction.Amount),
 			'description': 	hdfcTransaction.Description,
 		}
-		
 		result_1 = self.Repository.ExecuteQuery(SqlCommand(query_1, params_1))
 		
-		return (len(result_1) > 0)
+		query_2 = "SELECT * FROM [HdfcTransactions] WHERE [ValueDate]=:valueDate AND [DebitAmount]=:debitAmount AND [CreditAmount]=:creditAmount AND [ChqRefNumber]=:chqRefNumber AND [ClosingBalance]=:closingBalance;"
+		params_2 = {
+			'valueDate': hdfcTransaction.ValueDate.isoformat(),
+			'debitAmount': hdfcTransaction.DebitAmount,
+			'creditAmount': hdfcTransaction.CreditAmount,
+			'chqRefNumber': hdfcTransaction.ChqRefNumber,
+			'closingBalance': hdfcTransaction.ClosingBalance,
+		}
+		result_2 = self.Repository.ExecuteQuery(SqlCommand(query_2, params_2))
+		
+		return ((len(result_1) > 0) and len(result_2) > 0) # i.e. the whole transaction already exists in DB
